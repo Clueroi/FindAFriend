55935c572ac6bdf0ec9aed0969da78440b4d8090
unknown <eriicsouzaromeroo@gmail.com>
2024-09-30 15:44:52 -0300

    Add:JWT to code

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,32 @@
+import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
+import { ZodError } from "zod";
+
 import { RoutesUser } from "./http/controllers/user/routes";
 import { RoutesOrganization } from "./http/controllers/organization/routes";
-import { ZodError } from "zod";
+
 import { env } from "./env";
+
+
 export const app = fastify()
 
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET
+})
 
 app.register(RoutesUser)
 app.register(RoutesOrganization)
 
-app.setErrorHandler((error, _, reply)=>{
-    if(error instanceof ZodError){
-        return reply.status(400).send({message:'Validation Error', issues:error.format()})
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({ message: 'Validation Error', issues: error.format() })
     }
 
-    if(env.NODE_ENV !== 'production'){
+    if (env.NODE_ENV !== 'production') {
         console.error(error)
     } else {
         // Use some tool to oservate what's going on with your code
     }
 
-    return reply.status(500).send({message:'Internal Server Error '})
+    return reply.status(500).send({ message: 'Internal Server Error ' })
 })
