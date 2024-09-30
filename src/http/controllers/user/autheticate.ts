d6474e4a05613a2efdbdbb75867b7f3aaba9aa21
unknown <eriicsouzaromeroo@gmail.com>
2024-09-30 15:47:27 -0300

    Chore: Eslint to auth

--- src/http/controllers/user/autheticate.ts
+++ src/http/controllers/user/autheticate.ts
@@ -4,31 +4,31 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 
-export async function AuthenticateUser(request:FastifyRequest, reply:FastifyReply){
+export async function AuthenticateUser(request: FastifyRequest, reply: FastifyReply) {
     const authenticateSchemaBody = z.object({
-        email:z.string().email(),
-        password:z.string()
+        email: z.string().email(),
+        password: z.string()
     })
 
-    const {email, password} = authenticateSchemaBody.parse(request.body)
+    const { email, password } = authenticateSchemaBody.parse(request.body)
 
-    try{
-        const authenticateUser = makeAuthenticateUserUseCase() 
-    
-        authenticateUser.execute({
+    try {
+        const authenticateUser = makeAuthenticateUserUseCase()
+
+        const { user } = await authenticateUser.execute({
             email,
             password
         })
 
         reply.status(200).send()
 
-        
-    } catch(err){
-        if(err instanceof InvalidCredentialsError){
-            return reply.status(409).send({message:err.message})
+
+    } catch (err) {
+        if (err instanceof InvalidCredentialsError) {
+            return reply.status(409).send({ message: err.message })
         }
 
-        return reply.status(500).send({message:'Internal server error'})
+        return reply.status(500).send({ message: 'Internal server error' })
     }
 
 }
